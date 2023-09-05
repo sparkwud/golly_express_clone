d761744c99854b5724e3aff038b63c6a3b356b0a
sparkwud <kelvinagbenyo@gmail.com>
2023-09-05 21:06:46 +0000

    cleaner login screen

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,35 +1,43 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final gollyExpressLogo =
+        Image.asset("assets/images/golly_express_logo.png");
+    final googleLogo = SvgPicture.asset("assets/images/google_logo.svg");
+    final appleLogo = SvgPicture.asset("assets/images/apple_logo.svg");
     return GestureDetector(
       onTap: () {
         FocusManager.instance.primaryFocus?.unfocus();
       },
       child: Padding(
-        padding: EdgeInsets.only(top: 100.0),
+        padding: EdgeInsets.only(top: 80.0),
         child: Center(
           // heading text
           child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 16.0),
             child: SingleChildScrollView(
+              reverse: true,
               child: Column(
                 children: [
+                  gollyExpressLogo,
                   Text(
                     "Let's deliver for you",
                     style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
+                      fontWeight: FontWeight.w800,
                     ),
                   ),
                   Text(
                     "Register or sign up and we'll get you started",
                     style: TextStyle(
-                      fontSize: 15,
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
                     ),
                   ),
                   SizedBox(
@@ -106,12 +114,18 @@ class LoginScreen extends StatelessWidget {
                         color: Colors.grey.shade300,
                       ),
                     ),
-                    child: Text(
-                      "Continue with Google",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 16,
-                      ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        googleLogo,
+                        Text(
+                          "Continue with Google",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                   SizedBox(height: 20),
@@ -127,12 +141,18 @@ class LoginScreen extends StatelessWidget {
                         color: Colors.grey.shade300,
                       ),
                     ),
-                    child: Text(
-                      "Sign up with Apple",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 16,
-                      ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        appleLogo,
+                        Text(
+                          "Sign up with Apple",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                   Row(
